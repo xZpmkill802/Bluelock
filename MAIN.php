ae82e056d57e6f050ede0f259d1452241f415c28
xZpmkill802 <xzpmkillz@gmail.com>
2025-03-02 16:02:02 +0700

    Update MAIN.php

--- MAIN.php
+++ MAIN.php
@@ -1,5 +1,5 @@
 local key = _G.Key
-local check = "https://raw.githubusercontent.com/xZpmkill802/Bluelock/refs/heads/main/Check.php?key=" .. key
+local check = "https://raw.githubusercontent.com/xZpmkill802/Bluelock/refs/heads/main/1.lua?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xZpmkill802/Bluelock/refs/heads/main/Script.lua"))()
 else
