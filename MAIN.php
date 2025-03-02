aed192f3368b101786b1dd78239e1b0ad81b8068
xZpmkill802 <xzpmkillz@gmail.com>
2025-03-02 15:59:06 +0700

    Update MAIN.php

--- MAIN.php
+++ MAIN.php
@@ -1,7 +1,7 @@
 local key = _G.Key
 local check = "https://raw.githubusercontent.com/xZpmkill802/Bluelock/refs/heads/main/Check.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("scripthere"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xZpmkill802/Bluelock/refs/heads/main/Script.lua"))()
 else
-game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
+game.Players.LocalPlayer:Kick("ไปซื้อ Key ไป : Back to buy Key here")
 end
